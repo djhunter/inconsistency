b0cfa8101bb7adb6c4baa5938e4872aa8ff02a79
David J. Hunter <dhunter@westmont.edu>
2018-01-24 12:31:07 -0800

    Tweaked figures for paper.

--- zonesize3.R
+++ zonesize3.R
@@ -34,9 +34,23 @@ cat("Finished. Processed", i, "umpires.\n")
 # Figures for the paper:
 # xtable(cor(umpdf[,c(11,7,8,6)]))
 
+umpdf <- readRDS("umpdf2017.Rda")
+library(ggfortify)
+rownames(umpdf) <- umpdf$umpName
 pca <- prcomp(umpdf[,c(6,11,9,12,10)],scale=TRUE)
 
 # Figures for the paper:
 # xtable(cor(umpdf[,c(11,7,8,6)]))
 # xtable(pca$rotation)
 # xtable(cor(umpdf[,c(6,11,9,12,10)]))
+
+ggplot(umpdf, aes(y=accKDE, x=aveincon, label=umpName))+xlim(0.0229,0.051)+
+   geom_point(alpha=0) +geom_text(aes(label=umpName),hjust=0.5, vjust=0.0, size=7)+
+   labs(y="Consensus Zone Accuracy", x = "Average Inconsistency Index")+
+   theme(axis.text=element_text(size=18), axis.title=element_text(size=24,face="bold"))
+ggsave("inconvacc.eps", device="eps", width=18, height=18)
+
+autoplot(pca, label=TRUE, label.size=7, label.hjust=0.5, label.vjust=0.0, shape=FALSE, xlim=c(-0.27,0.27),
+         xlab = "Strike Zone Sloppiness", ylab = "Hitter Friendliness")+
+         theme(axis.text=element_text(size=18),axis.title=element_text(size=24,face="bold"))
+ggsave("umpPCAscatter.eps", device="eps", width=18, height=18)
