72dc90acd8a22a05fe89b837d3fae86f64c64c7b
David J. Hunter <dhunter@westmont.edu>
2023-08-17 10:20:42 -0700

    Compare abs and challenge system zones

--- aaa/zoneshapes.R
+++ aaa/zoneshapes.R
@@ -28,25 +28,26 @@ allAAAcalls <- calledPitches %>%
   scale_color_manual(values = c("#355E3B", "#AA0000"))
 ggsave("docs/allAAAcalls.png", plot = allAAAcalls, width = 12, height = 10, units = "cm")  
 
-## Function to make contour plots for decision boundaries
-zoneContourPlot = function(b, s, cp, color = "black") {
-  StkKDE <- kde2d(s$px, s$pz, n = 200, lims = c(-2,2,0,5))
-  CpKDE <- kde2d(cp$px, cp$pz, n = 200, lims = c(-2,2,0,5))
+## Function to make contours for decision boundaries
+zoneContourDF = function(b, s, cp, 
+                         level=0.5, num_points = 200, limits = c(-2,2,0,5)) {
+  StkKDE <- kde2d(s$px, s$pz, n = num_points, lims = limits)
+  CpKDE <- kde2d(cp$px, cp$pz, n = num_points, lims = limits)
   CzKDE <- StkKDE
   CzKDE$z <- CzKDE$z/CpKDE$z*nrow(s)/nrow(cp)
-  Szcontour <- contourLines(CzKDE, levels=0.5)
+  Szcontour <- contourLines(CzKDE, levels=level)
   Szcontourdf <- data.frame(px = Szcontour[[1]]$x, pz = Szcontour[[1]]$y)
-  ZoneShape <- ggplot() +
-    geom_path(data=Szcontourdf, aes(x=px, y=pz), color=color) +
-    coord_fixed(xlim=c(-1.5,1.5), ylim=c(1.0,4)) +
-    theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank())
-  return(ZoneShape)  
+  return(Szcontourdf)  
 }
 
 balls <- calledPitches[calledPitches$des=="Ball" | calledPitches$des=="Ball In Dirt",
                        c("px", "pz", "stand")]
 strikes <- calledPitches[calledPitches$des=="Called Strike", c("px", "pz", "stand")]
-AAAzone <- zoneContourPlot(balls, strikes, calledPitches) +
+AAAABSzonedf <- zoneContourDF(balls, strikes, calledPitches)
+AAAzone <- ggplot(AAAABSzonedf) +
+  geom_path(aes(x=px, y=pz), color="black") +
+  coord_fixed(xlim=c(-1.5,1.5), ylim=c(1.0,4)) +
+  theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
   ggtitle("ABS Zone (AAA)")
 
 ## Now repeat for MLB season
@@ -62,8 +63,40 @@ MLBcalledPitches <- readRDS("aaa/allMLBgames2023.rds") %>% # scraped in getMLBda
 MLBballs <- MLBcalledPitches[MLBcalledPitches$des=="Ball" | MLBcalledPitches$des=="Ball In Dirt",
                        c("px", "pz", "stand")]
 MLBstrikes <- MLBcalledPitches[MLBcalledPitches$des=="Called Strike", c("px", "pz", "stand")]
-MLBzone <- zoneContourPlot(MLBballs, MLBstrikes, MLBcalledPitches) +
+MLBzone <- ggplot(zoneContourDF(MLBballs, MLBstrikes, MLBcalledPitches)) +
+  geom_path(aes(x=px, y=pz), color="black") +
+  coord_fixed(xlim=c(-1.5,1.5), ylim=c(1.0,4)) +
+  theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
   ggtitle("Human Umpire Zone (MLB)")
-
 ggsave("docs/ABSvsMLBzones.png", plot = AAAzone + MLBzone, width = 16, height = 8, units = "cm")
+
+cs_game_pbp <- all_game_pbp %>% # Challenge system in AAA
+  filter(wday(game_date, label = TRUE) %in% c("Fri", "Sat", "Sun"))
+cs_calledPitches <- cs_game_pbp %>%
+  filter(details.code %in% c("B", "*B", "C"),
+         !(is.na(pitchData.coordinates.pX) | is.na(pitchData.coordinates.pZ))) %>%
+  transmute(px = pitchData.coordinates.pX, 
+            pz = 2.0*(pitchData.coordinates.pZ - pitchData.strikeZoneTop)/
+              (pitchData.strikeZoneTop - pitchData.strikeZoneBottom) + 3.5,
+            des = details.description,
+            stand = matchup.batSide.code) 
+cs_balls <- calledPitches[calledPitches$des=="Ball" | calledPitches$des=="Ball In Dirt",
+                       c("px", "pz", "stand")]
+cs_strikes <- calledPitches[calledPitches$des=="Called Strike", c("px", "pz", "stand")]
+AAACSzonedf <- zoneContourDF(cs_balls, cs_strikes, cs_calledPitches) %>%
+  mutate(System = "Challenge")
+allAAAzonedf <- AAAABSzonedf %>%
+  mutate(System = "Automated") %>%
+  bind_rows(AAACSzonedf)
+ABSvsCS <- ggplot(allAAAzonedf) +
+  geom_path(aes(x = px, y = pz, color = System)) +
+  coord_fixed(xlim=c(-1.5,1.5), ylim=c(1.0,4)) +
+  theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
+  ggtitle("ABS vs. Challenge System")
+
+AAAzone <- ggplot(AAACSzonedf) +
+  geom_path(aes(x=px, y=pz), color="black") +
+  coord_fixed(xlim=c(-1.5,1.5), ylim=c(1.0,4)) +
+  theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
+  ggtitle("ABS Zone (AAA)")
 
